b7ae8e8ec301d6a219b2d7e35a65fe990abe71e7
giesta <47950470+giesta@users.noreply.github.com>
2020-07-18 10:44:59 +0300

    added methods to interface

--- WebApplication/WebApplication/Services/ToDoList/IInMemoryTodoItemProvider.cs
+++ WebApplication/WebApplication/Services/ToDoList/IInMemoryTodoItemProvider.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication.Models;
 
 namespace WebApplication.Services.ToDoList
 {
     interface IInMemoryToDoItemProvider
     {
+        List<ToDoItem> GetAll();
+        ToDoItem Get(int id);
+        void Add(ToDoItem toDoItem);
     }
 }
