7710b9e8ce12823bf0dd4b93d6dd49aac45de437
giesta <47950470+giesta@users.noreply.github.com>
2020-08-09 01:37:49 +0300

    added viewmodels

---
+++ WebApplication/ToDoList.Web/ViewModel/ToDoList/TagToDoItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ToDoList.Business.Models.ToDoList;
+
+namespace ToDoList.Web.ViewModel.ToDoList
+{
+    public class TagToDoItemViewModel
+    {
+        public int TagId { get; set; }
+        public TagDao TagDao { get; set; }
+
+        public int ToDoItemId { get; set; }
+        public ToDoItemViewModel ToDoItemDao { get; set; }
+    }
+}
---
+++ WebApplication/ToDoList.Web/ViewModel/ToDoList/TagViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using ToDoList.Business.Models.ToDoList;
+
+namespace ToDoList.Web.ViewModel.ToDoList
+{
+    public class TagViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public IList<TagToDoItemDao> TagToDoItems { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is CategoryDao category &&
+                   Id == category.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
