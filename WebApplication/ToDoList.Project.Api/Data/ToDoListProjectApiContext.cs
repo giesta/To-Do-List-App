092140b1d712e1c0e13097b9edaa754d304c5107
giesta <47950470+giesta@users.noreply.github.com>
2020-08-28 22:22:16 +0300

    created new EF db

---
+++ WebApplication/ToDoList.Project.Api/Data/ToDoListProjectApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Project.Api.Model;
+
+namespace ToDoList.Project.Api.Data
+{
+    public class ToDoListProjectApiContext : DbContext
+    {
+        public ToDoListProjectApiContext (DbContextOptions<ToDoListProjectApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ToDoList.Project.Api.Model.Client> Client { get; set; }
+    }
+}
