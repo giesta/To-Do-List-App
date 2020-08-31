414c81073e70ef57deae559bd1c435b5d0db12da
giesta <47950470+giesta@users.noreply.github.com>
2020-08-31 20:46:36 +0300

    created controller to view clients

---
+++ WebApplication/ToDoList.Web/Controllers/ClientsApiController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ToDoList.ProjectManage.ApiClient;
+
+namespace ToDoList.Web.Controllers
+{
+    public class ClientsApiController : Controller
+    {
+        private readonly ApiClient _projectManageApi;
+
+        public ClientsApiController(ILogger<HomeController> logger, ApiClient projectManageApi)
+        {
+            _projectManageApi = projectManageApi;
+        }
+        // GET: ClientsApi
+        public async Task<IActionResult> Index()
+        {
+            return View(await _projectManageApi.ClientsGetAsync());
+        }
+
+        // GET: ClientsApi/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await _projectManageApi.ClientsGetAsync(id));
+        }
+
+       
+    }
+}
